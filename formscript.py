d2b288e2d93b31f11ccd37e8782816d6ebd4de41
Adam Dillard <adamdill@usc.edu>
2020-04-08 01:29:57 -0500

    updates

--- formscript.py
+++ formscript.py
@@ -50,37 +50,46 @@ import cgi
 import cgitb
 cgitb.enable()
 
-conn = mysql.connector.connect(host='localhost',
-                                   database='authorize',
-                                   user='root',
-                                   password='adam')
-input_data=cgi.FieldStorage()
-
-print('<h1>Addition Results</h1>')
+conn = None
 try:
-  zipcode=int(input_data["zip"].value)
-  fullname= input_data["fname"].value
-  email = input_data["email"].value
-  address = input_data["address"].value
-  city = input_data["city"].value
-  state = input_data["state"].value
-  cardname = input_data["cardname"].value
-  cardnum = int(input_data["cardnum"].value)
-  expmonth = int(input_data["expmonth"].value)
-  expyear = int(input_data["expyear"].value)
-  cvv = int(input_data["cvv"].value)
+  conn = mysql.connector.connect(host='localhost',
+                                     database='authorize',
+                                     user='root',
+                                     password='adam')
   
-  sql_command = """INSERT INTO users (fullname, email, address, city, state, zip, name, card, month, year, cvv) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
-  
-  recordTuple = (fullname, email, address, city, state, zipcode, cardname, cardnum, expmonth, expyear, cvv)
-  
-  conn.execute(sql_command, recordTuple)
-  
-  conn.commit()
-except:
-  print('<p>Sorry, some forms are either incomplete or wrong format.</p>')
-#  return(1)
+  if conn.is_connected:
+    input_data=cgi.FieldStorage()
+    try:
+      zipcode=int(input_data["zip"].value)
+      fullname= input_data["fname"].value
+      email = input_data["email"].value
+      address = input_data["address"].value
+      city = input_data["city"].value
+      state = input_data["state"].value
+      cardname = input_data["cardname"].value
+      cardnum = int(input_data["cardnum"].value)
+      expmonth = int(input_data["expmonth"].value)
+      expyear = int(input_data["expyear"].value)
+      cvv = int(input_data["cvv"].value)
+      
+      sql_command = """INSERT INTO users (fullname, email, address, city, state, zip, name, card, month, year, cvv) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
+      
+      recordTuple = (fullname, email, address, city, state, zipcode, cardname, cardnum, expmonth, expyear, cvv)
+      
+      conn.execute(sql_command, recordTuple)
+      
+      conn.commit()
 
-print('<p>Thanks!</p>')
+    except Error as e:
+      print('<p>',e,'</p>')
+      print('<p>Sorry, some forms are either incomplete or wrong format.</p>')
+
+except Error as e:
+  print('<p>',e,'</p>')
+
+finally:
+    if conn is not None and conn.is_connected():
+        conn.close()
+        print('<p>Connection closed</p>')
 
 
