cf2bd2f0ba393726aa9fec2f60d6ac676aa674a8
Adam Dillard <adamdill@usc.edu>
2020-04-28 22:16:37 -0500

    google anal

--- emailcheck.py
+++ emailcheck.py
@@ -4,46 +4,6 @@
 print('Content-Type:text/html') #HTML is following
 print("")                          #Leave a blank line
 
-###########################################################################
-#
-#import mysql.connector
-#from mysql.connector import Error
-#
-#conn = None
-#try:
-#    conn = mysql.connector.connect(host='localhost',
-#                                   database='autorize',
-#                                   user='root',
-#                                   password='adam')
-#    if conn.is_connected():
-#        print('<p>Connected to MySQL database</p>')
-#########
-#        try:
-#            cursor = conn.cursor()
-#            cursor.execute("SELECT * FROM users")
-#            row = cursor.fetchone()
-#            while row is not None:
-#                print('<p>',row,'</p>')
-#                row = cursor.fetchone()
-#        except Error as e:
-#            print('<p>',e,'</p>')
-#        finally:
-#            cursor.close()
-#            print('<p>Cursor closed</p>')
-#########
-#    else:
-#        print('<p>Unable to connect to MySQL database</p>')
-#
-#except Error as e:
-#        print('<p>',e,'</p>')
-#
-#finally:
-#    if conn is not None and conn.is_connected():
-#        conn.close()
-#        print('<p>Connection closed</p>')
-
-###########################################################################
-
 import mysql.connector
 from mysql.connector import Error
 import cgi
@@ -76,12 +36,20 @@ try:
         print('Email not associated with subscription')
       else:
         #need alert
+        try:
+
+          sql = """UPDATE users SET sub = %s WHERE email = %s"""
+          varr = (0, email)
+          cursor.execute(sql, varr)
+          conn.commit()
+        except Error as e:
+          print('<p>',e,'</p>')
+
         print('Subscription cancelled')
 
     except Error as e:
       print('<p>',e,'</p>')
       
-
     finally:
       print('<p>Done!</p>')
       cursor.close()
